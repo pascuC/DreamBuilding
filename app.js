721fecf624c821937f3358031595042dc7c5180b
pascuC <pascu97c@gmail.com>
2020-12-09 03:07:47 +0200

    review route restructed + adde .env configuration

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+if(process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 const express = require("express");
 const bodyParser = require('body-parser')
 const mongoose  = require("mongoose");
 const path = require('path');
 const ejsMate = require('ejs-mate');
-const { reviewSchema } = require('./schemas.js');
-const ExpressError = require('./ExpressError');
-const Review = require('./models/review');
+
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/dreambuilding', {
+const reviews = require('./routes/reviews')
+
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
@@ -25,43 +29,14 @@ app.engine('ejs', ejsMate);
 app.use(express.static(__dirname + '/views'));
 app.use(bodyParser.urlencoded({extended: true})) 
 app.use(bodyParser.json()) 
+app.use('/', reviews);
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,'views'));
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg, 400);
-    } else {
-        next();
-    }
-}
-
-app.get('/', async (req, res) =>{
-    const reviews = await Review.find({}).populate();
-    try{
-        res.render('index', { reviews });
-    }
-    catch(e) {
-        res.status(404).send();
-    }
-});
-
-app.post('/review', validateReview, async(req, res, next)=>{
-    const review = new Review(req.body.review);
-    try{
-        await review.save();
-        res.redirect(`/`);
-    } catch (e) {
-        res.status(404).send(e);
-    }
-});
-
-
-app.listen(3000, () =>{
-    console.log("Server is listening on port 3000");
+const port = process.env.PORT;
+app.listen(port, () =>{
+    console.log("Server is listening on port " + port);
 });
---
+++ routes/reviews.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+
+const Review = require('../models/review');
+const { reviewSchema } = require('../schemas.js');
+
+const ExpressError = require('../ExpressError');
+
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
+router.get('/', async (req, res) =>{
+    const reviews = await Review.find({}).populate();
+    try{
+        res.render('index', { reviews });
+    }
+    catch(e) {
+        res.status(404).send();
+    }
+});
+
+router.post('/review', validateReview, async(req, res, next)=>{
+    const review = new Review(req.body.review);
+    try{
+        await review.save();
+        res.redirect(`/`);
+    } catch (e) {
+        res.status(404).send(e);
+    }
+});
+
+module.exports = router;
